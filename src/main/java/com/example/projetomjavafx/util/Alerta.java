68dfb58de0ba5786622b63b58a010d8ebc77c853
Aliszson <aligbr150@gmail.com>
2024-09-05 11:01:32 -0300

    Adicionando classe de Alerta

---
+++ src/main/java/com/example/projetomjavafx/util/Alerta.java
@@ -0,0 +1,14 @@
+// Classe de utilidade para exibir alertas na tela
+
+package com.example.projetomjavafx.util;
+import javafx.scene.control.Alert;
+
+public class Alerta {
+    public static void exibirAlerta(String title, String header, String content, Alert.AlertType type) {
+        Alert alerta = new Alert(type);
+        alerta.setTitle(title);
+        alerta.setHeaderText(header);
+        alerta.setContentText(content);
+        alerta.show();
+    }
+}
