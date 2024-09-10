72e11448814a8d4af33f6962f9fd1aa83a0feef8
fcolucas <franciscoolucas@hotmail.com>
2024-09-10 12:22:59 -0300

    Funcionalidade de login do usuário

--- src/main/java/com/example/projetomjavafx/ApplicationController.java
+++ src/main/java/com/example/projetomjavafx/ApplicationController.java
@@ -12,11 +12,25 @@ public class ApplicationController {
     @FXML
     private Button botaoRegistrar;
     @FXML
+    private Button botaoLogar;
+    @FXML
     void onBotaoRegistrarClick(){
         try {
             ApplicationController.setStage((Stage) botaoRegistrar.getScene().getWindow());
             Application.updateStageScene(getStage(), "registrar_usuario-view.fxml");
             stage.setTitle("Registrar");
+            stage.setResizable(false);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @FXML
+    void onBotaoLogarClick (){
+        try {
+            ApplicationController.setStage((Stage) botaoLogar.getScene().getWindow());
+            Application.updateStageScene(getStage(), "login_usuario-view.fxml");
+            stage.setTitle("Logar");
             stage.setResizable(false);
         } catch (IOException e) {
             throw new RuntimeException(e);
---
+++ src/main/java/com/example/projetomjavafx/LoginUsuarioController.java
@@ -0,0 +1,76 @@
+package com.example.projetomjavafx;
+
+import com.example.projetomjavafx.model.dao.DaoFactory;
+import com.example.projetomjavafx.util.Alerta;
+import com.example.projetomjavafx.util.Restricoes;
+import com.example.projetomjavafx.util.SessaoUsuario;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.ResourceBundle;
+
+public class LoginUsuarioController implements Initializable {
+
+    @FXML
+    private ImageView iconeVoltar;
+    @FXML
+    private TextField nome;
+    @FXML
+    private PasswordField senha;
+    @FXML
+    private Button entrar;
+
+    @FXML
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Restricoes.verificaNome(nome);
+        Restricoes.verificaSenha(senha);
+
+    }
+
+    @FXML
+    void onIconeVoltarClick(){
+        try{
+            Application.updateStageScene(ApplicationController.getStage(), "application-view.fxml");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @FXML
+    void onEntrarClick(){
+        List<String> listaErros = new ArrayList<>();
+
+        if (Objects.equals(nome.getText(), "") || Objects.equals(senha.getText(), "")) {
+            listaErros.add("- Os campos de nome e senha não podem ser vazios.");
+        }else if (DaoFactory.createUsuarioDao().login(nome.getText(), senha.getText())== null){
+            listaErros.add("- Nome de usuário o senha inválidos");
+        }
+
+        if(!listaErros.isEmpty()){
+            String erros = String.join("\n", listaErros);
+            Alerta.exibirAlerta("Erro", "Campo inválido", erros , Alert.AlertType.ERROR);
+            return;
+        }
+
+        SessaoUsuario sessao = new SessaoUsuario();
+        sessao.setUsuario(DaoFactory.createUsuarioDao().login(nome.getText(), senha.getText()));
+        
+        try{
+            Application.updateStageScene(ApplicationController.getStage(), "telaprincipal-view.fxml");
+        }catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
+
+
