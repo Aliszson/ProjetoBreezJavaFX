986a900c3e06dcb78ef9e21d294bcb945afd64be
Aliszson <aligbr150@gmail.com>
2024-09-10 22:35:26 -0300

    Adicionando controller tela principal

--- src/main/java/com/example/projetomjavafx/TelaPrincipalController.java
+++ src/main/java/com/example/projetomjavafx/TelaPrincipalController.java
@@ -1,4 +1,20 @@
 package com.example.projetomjavafx;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.FlowPane;
+
 public class TelaPrincipalController {
+    @FXML
+    private AnchorPane moldeAlbum;
+    @FXML
+    private FlowPane exbicaoAlbuns;
+    @FXML
+    private Button acao;
+
+    @FXML
+    protected void teste(){
+        exbicaoAlbuns.getChildren().add(moldeAlbum);
+    }
 }
